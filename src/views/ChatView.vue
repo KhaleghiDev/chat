4e5cd788c65cc883d6cac332fdc0b32f441732b7
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2023-04-27 07:05:24 +0430

    set header

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,26 +1,28 @@
 <script setup>
-import TheChat from '../components/TheChat.vue'
-import axios from 'axios'
-function listchat(){
-  axios.get('http://localhost/openAi/api/chat/project/list')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-}
-listchat
+import TheChat from "../components/TheChat.vue";
+import axios from "axios";
+function listchat() {
+  axios
+    .get("http://openai.test/api/chat/project/list", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+    
+listchat;
 </script>
 <template>
-   
-      <TheChat/>
-
-  </template>
+  <TheChat />
+</template>
   
   <style>
-
-  </style>
+</style>
   
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    devServer: {
+      proxy: 'http://openai.test/',
     }
   }
 })
